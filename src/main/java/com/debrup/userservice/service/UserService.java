befeb532c438e8713cb63f8c83b49413ed715625
Debrup Mondal <debrup.official.365@gmail.com>
2021-08-05 12:34:52 +0530

    service implemented

---
+++ src/main/java/com/debrup/userservice/service/UserService.java
@@ -0,0 +1,14 @@
+package com.debrup.userservice.service;
+
+import com.debrup.userservice.model.Role;
+import com.debrup.userservice.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+}
