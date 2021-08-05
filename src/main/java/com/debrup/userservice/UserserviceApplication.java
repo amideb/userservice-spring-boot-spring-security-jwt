4749419f2f5ad9a568fdbf5ad535d5b88c8d5ce8
Debrup Mondal <debrup.official.365@gmail.com>
2021-08-05 14:21:31 +0530

    CommandLineRunner implemented for role management

--- src/main/java/com/debrup/userservice/UserserviceApplication.java
+++ src/main/java/com/debrup/userservice/UserserviceApplication.java
@@ -1,13 +1,40 @@
 package com.debrup.userservice;
 
+import com.debrup.userservice.model.Role;
+import com.debrup.userservice.model.User;
+import com.debrup.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
+    }
+
+    CommandLineRunner run(UserService userService){
+        return arg ->{
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+            userService.addRoleToUser("john", "ROLE_USER");
+            userService.addRoleToUser("will", "ROLE_MANAGER");
+            userService.addRoleToUser("jim", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_USER");
+        };
     }
 
 }
